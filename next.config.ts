c163da828aae8252e45532aeb28cb1255531dc5e
dhanesh1232 <dhaneshreddy980@gmail.com>
2026-05-17 00:27:27 +0530

    chore: update image remote patterns to include cdn.ecodrix.com and consolidate r2.dev hostnames

--- next.config.ts
+++ next.config.ts
@@ -14,12 +14,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "pub-3cc75b1385c24738b917d6e22805ff4b.r2.dev",
+        hostname: "*.r2.dev",
       },
       {
         protocol: "https",
-        hostname: "*.r2.dev",
-      },
+        hostname: "cdn.ecodrix.com",
+      }
     ],
   },
   experimental: {
